591015d8a3d9442c3de208f91e3604b447b63d03
lui7z <luiz727@gmail.com>
2025-02-14 20:11:19 -0300

    a

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Se você quiser começar a medir o desempenho em seu aplicativo, passe uma função
+// para registrar resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint de análise. Saiba mais: https://bit.ly/CRA-vitals
 reportWebVitals();
